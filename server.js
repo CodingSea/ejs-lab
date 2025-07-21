9101e898cc082ed14273bade6d0a0e8d1cfef33d
FahadQ <Fhdq2003@email.com>
2025-07-21 14:46:38 +0300

    finished setup

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+{
+  res.send('Hello There!');
+});
+
+app.listen(3000);
